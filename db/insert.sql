751870602a0e51c8a39e9019e7f65a8a50277abf
Katrin Valge <katrin.valge@mail.ee>
2023-02-05 19:17:04 +0200

    Created db insert file with sql script.

---
+++ db/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Action');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Crime');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Horror');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Documentary');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Comedy');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Horror');
+
+INSERT INTO movie (id, name)
+VALUES (DEFAULT, 'Drama');
