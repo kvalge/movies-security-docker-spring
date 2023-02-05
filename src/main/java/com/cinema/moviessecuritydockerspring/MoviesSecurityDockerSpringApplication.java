cce1468db47462fe0eec3388836cef1e5d09b45b
Katrin Valge <katrin.valge@mail.ee>
2023-02-05 18:03:00 +0200

    Initial commit.

---
+++ src/main/java/com/cinema/moviessecuritydockerspring/MoviesSecurityDockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.moviessecuritydockerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSecurityDockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSecurityDockerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinema/moviessecuritydockerspring/MoviesSecurityDockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.moviessecuritydockerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSecurityDockerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
