d0f4ddad85f7bf20ab3fb79d3cb41f57ed13298c
Katrin Valge <katrin.valge@mail.ee>
2023-02-07 23:02:49 +0200

    Added @CrossOrigin annotation. Updated insert.sql.

--- db/insert.sql
+++ db/insert.sql
@@ -25,3 +25,6 @@ VALUES (DEFAULT, 'Man on Fire', 'John Creasy, a despondent, alcoholic, former CI
 
 INSERT INTO category (id, name, description, category_id)
 VALUES (DEFAULT, 'General Magic', 'In 1990, at a secretive Silicon Valley startup, a small and passionate group of engineers and visionaries formed one of history’s greatest tech teams to build a magical device that would enable anyone to connect everyone, everywhere to everything.', 2);
+
+INSERT INTO category (id, name, description, category_id)
+VALUES (DEFAULT, 'Another Round', 'Four friends decide to embark on an experiment to test Skårderud''s theory. They start a group log of what occurs when they start drinking at regular intervals to maintain this blood alcohol level.', 3);
--- src/main/java/com/cinema/moviessecuritydockerspring/domain/movie/MovieController.java
+++ src/main/java/com/cinema/moviessecuritydockerspring/domain/movie/MovieController.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
+@CrossOrigin
 public class MovieController {
 
     @Resource
