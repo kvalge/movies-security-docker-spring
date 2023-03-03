0a6711036a2ea5b65fd2b80203ea9a0bb8cf390e
Katrin Valge <katrin.valge@mail.ee>
2023-03-03 21:28:21 +0200

    Created getAllUsers method test.

---
+++ src/test/java/com/cinema/moviessecuritydockerspring/domain/user/UserServiceTest.java
@@ -0,0 +1,94 @@
+package com.cinema.moviessecuritydockerspring.domain.user;
+
+import com.cinema.moviessecuritydockerspring.domain.role.Role;
+import com.cinema.moviessecuritydockerspring.domain.role.RoleRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    /**
+     * Checks if getAllUsers returns same user name and email as user name and email saved to database
+     * via user repository save method and
+     */
+    @Test
+    void getAllUsers() {
+        Role role = getRole();
+        Set<Role> roles = getRoles();
+        User user = getUser(roles);
+        String userName = user.getName();
+        String userEmail = user.getEmail();
+
+        List<UserResponse> users = userService.getAllUsers();
+        for (UserResponse userResponse : users) {
+            if (userName.equals(userResponse.getName()) && userEmail.equals(userResponse.getEmail())) {
+                assertTrue(true);
+            }
+        }
+
+        deleteUser(user);
+        deleteRole(role);
+    }
+
+    @Test
+    void getUserByUsername() {
+    }
+
+    @Test
+    void deleteUserByUsername() {
+    }
+
+    /**
+     * Hard coded role entity.
+     */
+    private Role getRole() {
+        Role role = new Role();
+        role.setName("ROLE_Roll");
+        return role;
+    }
+
+    private Set<Role> getRoles() {
+        Set<Role> roles = new HashSet<>();
+        roles.add(getRole());
+        return roles;
+    }
+
+    /**
+     * Hard coded user entity saved to database.
+     */
+    private User getUser(Set<Role> roles) {
+        User user = new User();
+        user.setName("Nimi");
+        user.setUsername("Kasutaja");
+        user.setEmail("Email");
+        user.setPassword("Salasõna");
+        user.setRoles(roles);
+        userRepository.save(user);
+        return user;
+    }
+
+    private void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
+    private void deleteRole(Role role) {
+        roleRepository.delete(role);
+    }
+}
