67737e551d30534ceb98273d1dea502e90555048
Katrin Valge <katrin.valge@mail.ee>
2023-03-05 17:53:41 +0200

    Created findByName method test with @DataJpaTest.

--- src/main/java/com/cinema/moviessecuritydockerspring/domain/movie/MovieRepository.java
+++ src/main/java/com/cinema/moviessecuritydockerspring/domain/movie/MovieRepository.java
@@ -2,11 +2,7 @@ package com.cinema.moviessecuritydockerspring.domain.movie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     Movie findByName(String name);
-
-    List<Movie> findByCategory(String name);
 }
---
+++ src/test/java/com/cinema/moviessecuritydockerspring/domain/movie/MovieRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.cinema.moviessecuritydockerspring.domain.movie;
+
+import com.cinema.moviessecuritydockerspring.domain.category.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MovieRepositoryTest {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @BeforeEach
+    void setup() {
+        Category categoryEntity = new Category();
+        categoryEntity.setName("Kategooria");
+
+        Movie movieEntity = new Movie();
+        movieEntity.setName("Film");
+        movieEntity.setDescription("Kirjeldus");
+        movieEntity.setCategory(categoryEntity);
+
+        testEntityManager.persist(categoryEntity);
+        testEntityManager.persist(movieEntity);
+    }
+
+    @Test
+    void findByName() {
+        Movie movie = movieRepository.findByName("Film");
+        assertEquals("Film", movie.getName());
+    }
+}
