caca8c9a54ae62e68c66cfc797394c5435de72b7
Katrin Valge <katrin.valge@mail.ee>
2023-03-11 19:30:44 +0200

    Updated logic and comments.

--- src/main/java/com/cinema/moviessecuritydockerspring/domain/price/PriceService.java
+++ src/main/java/com/cinema/moviessecuritydockerspring/domain/price/PriceService.java
@@ -17,14 +17,20 @@ public class PriceService {
 
     public static final String PRICE_6 = "6.00";
     public static final String PRICE_3 = "3.00";
+    public static final String PRICE_2 = "2.00";
     public static final String PRICE_FREE = "Free";
-    public static final String YEAR2015 = "2015";
+    public static final String YEAR2020 = "2020";
+    public static final String YEAR2005 = "2005";
 
     @Resource
     private AnalyticsService analyticsService;
 
     /**
      * Sets movie price according to the number of rentals and the release year.
+     * If the release year is after 2020, the price is 6.00.
+     * if the release year is after 2005 and the number of rentals is more than or equal to 490, the price is 3.00.
+     * if the release year is after 2005 and the number of rentals is less than 490, the price is 2.00.
+     * All the rest of movies are for free.
      */
     public PriceResponse setPrice(String movieName) throws ParseException {
         List<AnalyticsMovieResponse> movieAnalytics = analyticsService.getMovieAnalytics();
@@ -42,10 +48,12 @@ public class PriceService {
             Date year = formatter.parse(releaseYear);
 
             if (equalsToMovie) {
-                if ((movieRentals < 5 || movieRentals >= 15) && year.after(Date.from(Instant.parse(YEAR2015)))) {
+                if (year.after(Date.from(Instant.parse(YEAR2020)))) {
                     price = PRICE_6;
-                } else if (movieRentals < 15 && year.after(Date.from(Instant.parse(YEAR2015)))) {
+                } else if (movieRentals >= 490 && year.after(Date.from(Instant.parse(YEAR2005)))) {
                     price = PRICE_3;
+                } else if (movieRentals < 490 && year.after(Date.from(Instant.parse(YEAR2005)))) {
+                    price = PRICE_2;
                 } else {
                     price = PRICE_FREE;
                 }
