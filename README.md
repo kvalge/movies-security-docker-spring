dfd6d1415d0f0aa223aa72850fc15f8cfcdaa5f6
Katrin Valge <katrin.valge@mail.ee>
2023-03-12 19:01:51 +0200

    Updated price calculations. Updated README.md.

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ javadoc directory with index.html file to get general overview of the project.
 category: for movie categories (genres) with add, get all, get by name and delete functionalities.  
 movie: add, get by name, get all, update and delete functionalities.  
 moviedetails: to add and manage detailed info to movies.  
+price: calculates movie price according to release year and statistics of rentals.
 role: add new and delete functionalities. Roles are ADMIN and USER.  
 user: get all, get by username and delete functionalities.   
 register: register user functionality.  
--- src/main/java/com/cinema/moviessecuritydockerspring/domain/price/PriceService.java
+++ src/main/java/com/cinema/moviessecuritydockerspring/domain/price/PriceService.java
@@ -5,12 +5,8 @@ import com.cinema.moviessecuritydockerspring.analytics.AnalyticsService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Date;
+import java.time.Year;
 import java.util.List;
-import java.util.Locale;
 
 @Service
 public class PriceService {
@@ -19,20 +15,20 @@ public class PriceService {
     public static final String PRICE_3 = "3.00";
     public static final String PRICE_2 = "2.00";
     public static final String PRICE_FREE = "Free";
-    public static final String YEAR2020 = "2020";
-    public static final String YEAR2005 = "2005";
+    public static final int YEAR_SUBTRACT_5 = 5;
+    public static final int YEAR_SUBTRACT_9 = 9;
 
     @Resource
     private AnalyticsService analyticsService;
 
     /**
      * Sets movie price according to the number of rentals and the release year.
-     * If the release year is after 2020, the price is 6.00.
-     * if the release year is after 2005 and the number of rentals is more than or equal to 490, the price is 3.00.
-     * if the release year is after 2005 and the number of rentals is less than 490, the price is 2.00.
+     * If the release year is less than 5 years from the current year, the price is 6.00.
+     * if the release year is less than 9 year from the current year and the number of rentals is more than or equal to 490, the price is 3.00.
+     * if the release year is less than 9 year from the current year and the number of rentals is less than 490, the price is 2.00.
      * All the rest of movies are for free.
      */
-    public PriceResponse setPrice(String movieName) throws ParseException {
+    public PriceResponse setPrice(String movieName) {
         List<AnalyticsMovieResponse> movieAnalytics = analyticsService.getMovieAnalytics();
 
         PriceResponse priceResponse = new PriceResponse();
@@ -44,15 +40,17 @@ public class PriceService {
             String releaseYear = response.getReleaseYear();
             Integer movieRentals = response.getMovieRentals();
 
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy", Locale.ENGLISH);
-            Date year = formatter.parse(releaseYear);
+            Year year = Year.parse(releaseYear);
+            Year currentYear = Year.now();
+            Year minus5Years = currentYear.minusYears(YEAR_SUBTRACT_5);
+            Year minus9Years = currentYear.minusYears(YEAR_SUBTRACT_9);
 
             if (equalsToMovie) {
-                if (year.after(Date.from(Instant.parse(YEAR2020)))) {
+                if (year.isAfter(minus5Years)) {
                     price = PRICE_6;
-                } else if (movieRentals >= 490 && year.after(Date.from(Instant.parse(YEAR2005)))) {
+                } else if (year.isAfter(minus9Years) && movieRentals >= 490) {
                     price = PRICE_3;
-                } else if (movieRentals < 490 && year.after(Date.from(Instant.parse(YEAR2005)))) {
+                } else if (year.isAfter(minus9Years) && movieRentals < 490) {
                     price = PRICE_2;
                 } else {
                     price = PRICE_FREE;
